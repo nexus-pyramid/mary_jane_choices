dc717a1e5fb6cf32e2ffaac3353360a43b1b4ec6
Daniel Kouba <koubaphotog@gmail.com>
2017-03-31 19:13:46 -0700

    added votes over time to strains

--- server/models/strain.js
+++ server/models/strain.js
@@ -6,6 +6,8 @@ var StrainSchema = new Schema({
 	_business:[{type: Schema.Types.ObjectId, ref:'Business'}],
 	name: String,
 	type: String,
-	votes: {type: Number, default: 0}
+	votes: {type: Number, default: 0},
+	votes_over_time: [{type: Number}]
+
 });
 mongoose.model('Strain', StrainSchema);
