9af5370f40d90f3de9de21a4d4c1c5aefeed1c6d
Daniel Kouba <koubaphotog@gmail.com>
2017-04-12 15:10:02 -0700

    Fixed logout

--- public/assets/controllers/navController.js
+++ public/assets/controllers/navController.js
@@ -1,12 +1,20 @@
-app.controller('navController', function($rootScope, $scope, UserService){
+app.controller('navController', function($rootScope, $scope, UserService, UserFactory){
 
 	  $scope.UserService = UserService;
 
-	  $rootScope.$on('loggedin', function () {
-	  		$scope.UserService = UserService;
-		});
+	$rootScope.$on('loggedin', function () {
+  		$scope.UserService = UserService;
+	});
 
-	  $scope.logout = function(){
-	  	$scope.UserService = ''
-	  }
+	  
+	$scope.logout = function(){
+		$scope.UserService = '';
+		UserFactory.logout(function(data){
+			if(data['errors']){
+				console.log('Could not log out user')
+			} else {
+				console.log('')
+			}
+		})
+	}
 })
